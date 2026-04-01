8f463312296431eb4e34a4524df4dfc3a520aa40
Wagner Camargo <1wagnercamargo@gmail.com>
2026-03-31 22:42:05 -0300

    Retrive campaign details

--- public/pages/campaign/new.php
+++ public/pages/campaign/new.php
@@ -1,15 +1,6 @@
 <?php
-session_start();
 
 $title = 'Criar Nova Campanha';
+$view = '/var/www/app/views/campaign/new.phtml';
 
-// Recupera erros da sessão, se houver
-$errors = $_SESSION['errors'] ?? [];
-$title_value = $_SESSION['title_value'] ?? '';
-
-// Limpa a sessão após recuperar
-unset($_SESSION['errors'], $_SESSION['title_value']);
-
-$view = __DIR__ . '/../../../app/views/campaign/new.phtml';
-
-require __DIR__ . '/../../../app/views/layouts/application.phtml';
+require '/var/www/app/views/layouts/application.phtml';
---
+++ public/pages/campaign/show.php
@@ -0,0 +1,15 @@
+<?php
+
+$id = (int)$_GET['id'];
+
+define('DB_PATH', '/var/www/database/campaigns.txt');
+
+$campaigns = file(DB_PATH, FILE_IGNORE_NEW_LINES);
+
+$campaign['title'] = $campaigns[$id];
+
+$title = file_get_contents(DB_PATH);
+
+$view = '/var/www/app/views/campaign/show.phtml';
+
+require '/var/www/app/views/layouts/application.phtml';
