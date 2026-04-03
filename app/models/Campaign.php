ffaf4df42225d66abd6c5435a5759191bb96c261
Wagner Camargo Oliveira <32493077+olivertchai@users.noreply.github.com>
2026-04-03 13:49:17 -0300

    Refactor/crete and validations (#11)
    * Refactor the create to OO with no validations yet
    
    * Refactor create validations oo

---
+++ app/models/Campaign.php
@@ -0,0 +1,77 @@
+<?php
+
+class Campaign
+{
+    private const DB_PATH = '/var/www/database/campaigns.txt';
+    private array $errors = [];
+
+    private $id;
+    private $title;
+    private $description;
+    private DateTime $startDate;
+    private DateTime $endDate;
+    private $imagePath; // Novo atributo para o caminho da imagem
+
+    public function __construct($id, $title, $description, DateTime $startDate, DateTime $endDate, $imagePath = null)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->description = $description;
+        $this->startDate = $startDate;
+        $this->endDate = $endDate;
+        $this->imagePath = $imagePath;
+    }
+
+    public function getId(){return $this->id;}
+    public function getTitle(){return $this->title;}
+    public function getDescription(){return $this->description;}
+    public function getStartDate(){return $this->startDate;}
+    public function getEndDate(){return $this->endDate;}
+    public function getImagePath(){return $this->imagePath;}
+
+    public function setTitle($title){$this->title = $title;}
+    public function setDescription($description){$this->description = $description;}
+    public function setStartDate(DateTime $startDate){$this->startDate = $startDate;}
+    public function setEndDate(DateTime $endDate){$this->endDate = $endDate;}
+    public function setImagePath($imagePath){$this->imagePath = $imagePath;}
+
+    // Validação básica: data final não pode ser antes da inicial
+    public function validateDateInitialEnd()
+    {
+        if ($this->endDate < $this->startDate) {
+            throw new InvalidArgumentException("A data final não pode ser antes da data inicial.");
+        }
+    }
+
+    // Método para formatar a saída (ex: d/m/Y)
+    public function getIntervalFormated(string $format = 'd/m/Y'): string {
+        return $this->startDate->format($format) . ' até ' . $this->endDate->format($format);
+    }
+
+    public function save():bool{
+        if($this->isValid()) {
+            $this->id = uniqid(); // Gerar um ID único para a campanha
+            // Aqui você pode implementar a lógica para salvar a campanha em um banco de dados real
+            file_put_contents(self::DB_PATH, $this->title. PHP_EOL, FILE_APPEND);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function isValid():bool{
+        $this->errors = []; // Limpa erros anteriores
+
+        if(empty($this->title)) {
+            $this->errors[] = 'O título da campanha é obrigatório.';
+        }return empty($this->errors);
+    }
+
+    public function hasErrors(): bool{
+        return empty($this->errors);
+    }
+
+    public function getErrorsIndex($index): array{
+        return $this->errors[$index] ?? [];
+    }
+
+}
--- public/pages/campaign/create.php
+++ public/pages/campaign/create.php
@@ -1,5 +1,7 @@
 <?php
 
+require '/var/www/app/models/Campaign.php';
+
 $method = $_SERVER['REQUEST_METHOD'];
 
 // Redireciona para a página de listagem se o método não for POST
@@ -8,29 +10,52 @@ if ($method != 'POST') {
     exit();
 }
 
-define('DB_PATH', '/var/www/database/campaigns.txt');
+$params = $_POST['campaign'];
+$campaign = new Campaign(
+    id : uniqid(), // Gerar um ID único para a campanha
+    title : trim($params['title']),
+    description : trim($params['description']),
+    startDate : new DateTime($params['start_date']),
+    endDate : new DateTime($params['end_date'])
+);
 
-$campaign = $_POST['campaign'];
-$title = trim($campaign['title']);
-
-// VALIDAÇÕES SIMPLES
-$errors = [];
-
-if (empty($title)) {
-    $errors[] = 'O título da campanha é obrigatório.';
-}
-
-if (empty($errors)) {
-    // INSERINDO NO "BANCO DE DADOS" (ARQUIVO)
-    file_put_contents(DB_PATH, $title. PHP_EOL, FILE_APPEND);
-
-    header('Location: /pages/campaign');
-
-} else{
+if($campaign->save()) {
+    header('Location: /pages/campaign/index.php');
+} else {
     // Recarrega o formulário com os erros
-
     $title = 'Criar Nova Campanha';
     $view = '/var/www/app/views/campaign/new.phtml';
-
     require '/var/www/app/views/layouts/application.phtml';
 }
+
+
+
+
+
+
+// define('DB_PATH', '/var/www/database/campaigns.txt');
+
+// $campaign = $_POST['campaign'];
+// $title = trim($campaign['title']);
+
+// // VALIDAÇÕES SIMPLES
+// $errors = [];
+
+// if (empty($title)) {
+//     $errors[] = 'O título da campanha é obrigatório.';
+// }
+
+// if (empty($errors)) {
+//     // INSERINDO NO "BANCO DE DADOS" (ARQUIVO)
+//     file_put_contents(DB_PATH, $title. PHP_EOL, FILE_APPEND);
+
+//     header('Location: /pages/campaign');
+
+// } else{
+//     // Recarrega o formulário com os erros
+
+//     $title = 'Criar Nova Campanha';
+//     $view = '/var/www/app/views/campaign/new.phtml';
+
+//     require '/var/www/app/views/layouts/application.phtml';
+// }
