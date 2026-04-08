b12868ca2123e58e45b81f4ea60bdabd3e622cb8
Wagner Camargo <1wagnercamargo@gmail.com>
2026-04-08 00:13:14 -0300

    Write some tests controllers

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,3 +1,9 @@
 <?php
 
+require_once __DIR__ . '/../core/constants/general.php';
+
+require_once ROOT_PATH . '/core/env/env.php';
+require_once ROOT_PATH . '/core/debug/functions.php';
+require_once ROOT_PATH . '/core/errors/handlers.php';
+
 require __DIR__ . '/../vendor/autoload.php';
---
+++ tests/TestCase.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase as FrameworkTestCase;
+
+require dirname(__DIR__) . '/core/constants/general.php';
+require ROOT_PATH . '/core/debug/functions.php';
+
+class TestCase extends FrameworkTestCase
+{
+    public function setUp(): void
+    {
+        $this->clearDatabase(); // Limpa o banco de dados ou arquivos antes de cada teste, se necessário
+    }
+
+    public function tearDown(): void
+    {
+        // Limpa o banco de dados ou arquivos após cada teste, se necessário
+        $this->clearDatabase();
+    }
+
+    private function clearDatabase()
+    {
+        $file = DATABASE_PATH . $_ENV['DB_NAME'];
+        if (file_exists($file)) {
+            unlink($file); // Remove o arquivo de banco de dados
+        }
+    }
+}    
+
