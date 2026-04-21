29064be2385e6a6fb33912261bf59cba858bbdf3
Wagner Camargo Oliveira <32493077+olivertchai@users.noreply.github.com>
2026-04-20 23:59:19 -0300

    Chore/database integration (#28)
    * Setup initial routes
    
    * Setup named routes
    
    * Changed accept json to request class
    
    * Setup and development the use of query params at the router
    
    * Send an exception when the URI is not found. also get a better dd
    
    * Remove the folders pages
    
    * Upd the nginx server to direct the pages in index.php
    
    * Setup image postgres to docker compose
    
    * Setup PDO and PDO driver. also database sql
    
    * Development all CRUD using Mysql database with PDO
    
    * Setup tests to run with postgres
    
    * setup the populate

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,28 +2,20 @@
 
 namespace Tests;
 
-use Core\Constants\Constants;
+use Core\Database\Database;
 use PHPUnit\Framework\TestCase as FrameworkTestCase;
 
 class TestCase extends FrameworkTestCase
 {
     public function setUp(): void
     {
-        $this->clearDatabase(); // Limpa o banco de dados ou arquivos antes de cada teste, se necessário
+        Database::create();
+        Database::migrate();
     }
 
     public function tearDown(): void
     {
-        // Limpa o banco de dados ou arquivos após cada teste, se necessário
-        $this->clearDatabase();
-    }
-
-    private function clearDatabase(): void
-    {
-        $file = Constants::databasePath()->join($_ENV['DB_NAME']);
-        if (file_exists($file)) {
-            unlink($file); // Remove o arquivo de banco de dados
-        }
+        Database::drop();
     }
 
     protected function getOutput(callable $callable): string
