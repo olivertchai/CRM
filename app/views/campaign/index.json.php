95ca82caca12c65c72fabe9d13a36aec86d608c9
Wagner Camargo Oliveira <32493077+olivertchai@users.noreply.github.com>
2026-04-22 22:19:49 -0300

    fixed populate, fixed router init, return paginate info json (#30)

--- app/views/campaign/index.json.php
+++ app/views/campaign/index.json.php
@@ -7,3 +7,12 @@ foreach ($campaigns as $campaign) {
 }
 
 header('Content-Type: application/json');
+
+$json['campaigns'] = $campaignsToJson;
+$json['pagination'] = [
+    'page'                       => $paginator->getPage(),
+    'per_page'                   => $paginator->perPage(),
+    'total_of_pages'             => $paginator->totalOfPages(),
+    'total_of_registers'         => $paginator->totalOfRegisters(),
+    'total_of_registers_of_page' => $paginator->totalOfRegistersOfPage(),
+];
